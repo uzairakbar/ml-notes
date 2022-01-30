ab4a03a70108d5a55026a2414f447a1f37c392b6
uzairakbar <uzairakbar@outlook.com>
2022-01-30 16:45:41 +0000

    Update on Overleaf.

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,12 +5,16 @@ set -e
 echo "Creating output directory $OUT_DIR..."
 mkdir --parent $OUT_DIR
 
-for fileName in *.tex; do
+for fileName in *.tex *.cls; do
     [ -f "$fileName" ] || break
 
     echo "Substituting version number ${GITHUB_SHA::7} in file $fileName..."
     sed -i -e "s/verSubstitution/${GITHUB_SHA::7}/" $fileName
+done
 
+for fileName in *.tex; do
+    [ -f "$fileName" ] || break
+    
     echo "Converting file $fileName to pdf..."
     xelatex $fileName
 done
